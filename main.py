7731daef1d8f173ed3db69e17aa3bbfc2b3b6fac
george-jiang-wow <453617479@qq.com>
2023-04-29 05:57:06 +0000

    push

--- main.py
+++ main.py
@@ -1,50 +1,41 @@
 import requests as r
 import time as t
 import urllib.parse as u
-import csv
-import math
+import datetime
+#import csv
+#import math
 import os
 token = os.environ["token"]
-# 先实现登录
-f=open("a.txt","w")
-cookies={"__client_id":token,"_uid":"459900"}
+txt="""**请认准 OIso | OI搜 唯一官方机器人账号 ！**
+
+*仍在开发中*
+
+------
+
+今天的微博热搜：.weibotop.
+
+今天的日期：.date.
+
+------
+
+🍋OIso🔍 - 一款为 OIer 和开发者而生的搜索引擎
+
+https://www.oiso.cf/
+
+前端开发： @diyanqi @Lotuses
+
+后端开发： @diyanqi
+
+本 bot 可在洛谷上通知您的 OIso 消息以及一些推广。如果您不想继续接受这些消息，请回复 `td` 。如果您想恢复接收消息，请回复 `hf` 。"""
+cookies={"__client_id":token,"_uid":"464111"}
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36 Edg/94.0.992.47 BOTE"}
-got=r.get("https://www.luogu.com.cn/problem/list?type=P&page=1", headers=headers,cookies=cookies)
-msg=got.text.split('window._feInjection = JSON.parse(decodeURIComponent("')[1].split('            window._feConfigVersion =')[0]
-temp2=u.unquote(msg)
-fullpage=int(temp2.split('s":{"count":')[1].split(',"r')[0])
-print("done")
-for i in range(1,math.ceil(fullpage/50)+1):
-    got=r.get("https://www.luogu.com.cn/problem/list?type=P&page={}".format(i), headers=headers,cookies=cookies)
-    try:
-        for j in range(1,51):
-            msg=got.text.split('<li>')[j].split('&nbsp;<a')[0]
-            f.write(msg+"\n")
-    except:
-        print("done")
-    t.sleep(0.8)#这行是最最关键的一句代码，千万千万不要删除！
-f.close()
-data=[]
-f=open("a.txt","r")
-que=f.read().split("\n")
-for i in que:
-    if i=='':
-        break
-    got=r.get("https://www.luogu.com.cn/problem/solution/{}".format(i), headers=headers,cookies=cookies)
-    msg=got.text.split('window._feInjection = JSON.parse(decodeURIComponent("')[1].split('            window._feConfigVersion =')[0]
-    temp2=u.unquote(msg)
-    msg=temp2.split('},"acceptSolution":')[1].split('},"')[0]
-    t.sleep(0.8)#这行是最最关键的一句代码，千万千万不要删除！
-    got2=r.get("https://www.luogu.com.cn/problem/{}".format(i), headers=headers,cookies=cookies)
-    msg2=got2.text.split('window._feInjection = JSON.parse(decodeURIComponent("')[1].split('            window._feConfigVersion =')[0]
-    temp3=u.unquote(msg2)
-    if msg=="true":
-        data.append(["P",i,1,temp2.split('"difficulty":')[1].split(',')[0],temp3.split(',"tags":')[1].split(',"')[0].replace('"', '')])
-        print(i)
-    else:
-        data.append(["P",i,0,temp2.split('"difficulty":')[1].split(',')[0],temp3.split(',"tags":')[1].split(',"')[0].replace('"', '')])
-    t.sleep(0.8)#这行是最最关键的一句代码，千万千万不要删除！
-with open('data.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    for row in data:
-        writer.writerow(row)
+txt=txt.replace(".weibotop.",r.get("https://tenapi.cn/v2/weibohot").text.split('"name": "')[1].split('",')[0])
+txt=txt.replace(".date.",str(datetime.datetime.today()))
+first=r.get("https://v1.hitokoto.cn/").text.split('"hitokoto":"')[1].split('","typ')[0]
+re=r.get("https://www.luogu.com.cn/user/464111",headers=headers,cookies=cookies)
+headers["X-CSRF-TOKEN"]=re.text.split('name="csrf-token" content="')[1].split('">\n')[0]
+headers["Referer"]="https://www.luogu.com.cn/user/464111"
+re=r.post("https://www.luogu.com.cn/api/user/updateIntroduction",json={"introduction":txt},headers=headers,cookies=cookies)
+print(re)
+re=r.post("https://www.luogu.com.cn/api/user/updateSlogan",json={"slogan":"OIso | OI搜 唯一官方机器人账号 | "+first},headers=headers,cookies=cookies)
+print(re)
