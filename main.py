de79245c2b5110f3cc0c67b698ea533e85a74b1f
George Jiang <86469008+george-jiang-wow@users.noreply.github.com>
2023-04-29 14:24:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ txt="""**请认准 OIso | OI搜 唯一官方机器人账号 ！**
 
 更新时间（UTC）：.date.
 
+B站榜一：
+
+![](bilibili:.bilibili.)
+
 动态主页支持：@George_jiang
 
 ------
@@ -33,6 +37,7 @@ cookies={"__client_id":token,"_uid":"464111"}
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36 Edg/94.0.992.47 BOTE"}
 txt=txt.replace(".weibotop.",r.get("https://tenapi.cn/v2/weibohot").text.split('"name": "')[1].split('",')[0])
 txt=txt.replace(".date.",str(datetime.datetime.today()))
+txt=txt.replace(".bilibili.","av"+r.get("https://api.bilibili.com/x/web-interface/ranking/v2").text.split('"aid":')[1].split(',"videos"')[0])
 first=r.get("https://v1.hitokoto.cn/").text.split('"hitokoto":"')[1].split('","typ')[0]
 re=r.get("https://www.luogu.com.cn/user/464111",headers=headers,cookies=cookies)
 headers["X-CSRF-TOKEN"]=re.text.split('name="csrf-token" content="')[1].split('">\n')[0]
